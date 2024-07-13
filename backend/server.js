2e5881a9817b216f0e05b2b4ce3b2795a8945285
Shuva15 <shuvajitmahanta02@gmail.com>
2024-07-14 02:46:39 +0530

    implemented pyth oracle

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,27 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const { PriceServiceConnection } = require('@pythnetwork/price-service-client');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(cors());
+
+// Initialize Pyth connection
+const connection = new PriceServiceConnection("https://hermes.pyth.network");
+
+const priceId = "0xe62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43";
+
+// API endpoint to fetch the latest price
+app.get('/api/price', async (req, res) => {
+  try {
+    const prices = await connection.getLatestPriceFeeds([priceId]);
+    const price = prices[0].price.price / Math.pow(10, -prices[0].price.expo);
+    console.log(prices, price)
+    res.json({ price });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch price data' });
+  }
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,26 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [price, setPrice] = useState(null);
+
+  useEffect(() => {
+    const fetchPrice = async () => {
+      const response = await axios.get(`http://localhost:5000/api/price`);
+        setPrice(response.data.price);
+    };
+
+    fetchPrice();
+}, []);
+
   return (
-    <div className="App">
+    <div>
+      <div>
+        <h1>Oracle Aggregator</h1>
+          <div>
+            <h2 className="text-3xl font-semibold mb-2">BTC/USD: ${price}</h2>
+          </div>
+      </div>
     </div>
   );
 }
